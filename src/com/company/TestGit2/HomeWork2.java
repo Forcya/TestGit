f8bbd50c887892f533e507633b3104ac13f2ff6b
Forcya <74520011+Forcya@users.noreply.github.com>
2022-03-23 19:25:58 +0300

    Новыая ветка 3 и новый метод в ней

--- src/com/company/TestGit2/HomeWork2.java
+++ src/com/company/TestGit2/HomeWork2.java
@@ -4,5 +4,6 @@ public class HomeWork2 {
     public static void main(String[] args) {
         System.out.println("Тест 2 ай вай вай");
         System.out.println("Проверка на прочность");
+        System.out.println("Проверка на возврат");
     }
 }
